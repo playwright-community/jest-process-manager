287047ab9d8c2f3f08b162299ac758ac311ce12b
mmarkelov <maks-markel@mail.ru>
2020-05-20 15:46:30 +0300

    - Some ts improvements - Separate defaults and types

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,25 @@ import stream from 'stream'
 import net from 'net'
 import chalk from 'chalk'
 import cwd from 'cwd'
-import waitOn, { WaitOnOptions } from 'wait-on'
+import waitOn from 'wait-on'
 import findProcess from 'find-process'
 import { promisify } from 'util'
 import treeKill from 'tree-kill'
 import prompts from 'prompts'
 
-import { spawn, ChildProcess, SpawnOptions } from 'child_process'
+import { spawn } from 'child_process'
 import exit from 'exit'
 import onExit from 'signal-exit'
 
+import type { ChildProcess, SpawnOptions } from 'child_process'
+import type { CustomSpawnD, JestProcessManagerOptions } from './types';
+
+import { DEFAULT_CONFIG, ERROR_NO_COMMAND, ERROR_PORT_USED, ERROR_TIMEOUT } from './constants';
+
 const pTreeKill = promisify(treeKill)
 
-type CustomSpawnD = ReturnType<typeof spawn> & {
-  destroy: () => Promise<void>
-}
-
 function spawnd(command: string, options: SpawnOptions): CustomSpawnD {
-  const proc = spawn(command, options)
+  const proc = <CustomSpawnD>spawn(command, options)
   const cleanExit = (code = 1) => {
     if (proc && proc.pid) {
       treeKill(proc.pid, () => exit(code))
@@ -38,7 +39,7 @@ function spawnd(command: string, options: SpawnOptions): CustomSpawnD {
     cleanExit(typeof code === 'number' ? code : 1)
   });
 
-  (proc as CustomSpawnD).destroy = async (): Promise<void> => {
+  proc.destroy = async (): Promise<void> => {
     removeExitHandler()
     proc.removeAllListeners('exit')
     proc.removeAllListeners('error')
@@ -49,134 +50,6 @@ function spawnd(command: string, options: SpawnOptions): CustomSpawnD {
   return proc as CustomSpawnD
 }
 
-interface JestProcessManagerOptions {
-  /**
-   * Command to execute to start the port. Directly passed to spawnd.
-   *
-   * ```js
-   * module.exports = {
-   *   command: 'npm run start',
-   * }
-   * ```
-   */
-  command: string;
-
-  /**
-   * Log server output, useful if server is crashing at start.
-   * @default false
-   * ```js
-   * module.exports = {
-   *   command: 'npm run start',
-   *   debug: true,
-   * }
-   * ```
-   */
-  debug?: boolean;
-
-  /**
-   * How many milliseconds to wait for the spawned server to be available before giving up. Defaults to wait-port's default.
-   * @default 5000
-   * ```js
-   * module.exports = {
-   *   command: 'npm run start',
-   *   launchTimeout: 30000,
-   * }
-   * ```
-   */
-  launchTimeout?: number;
-
-  /**
-   * Host to wait for activity on before considering the server running. Must be used in conjunction with port.
-   * @default 'localhost'
-   *
-   * ```js
-   * module.exports = {
-   *   command: 'npm run start --port 3000',
-   *   host: 'customhost.com',
-   *   port: 3000
-   * }
-   * ```
-   */
-  host?: string;
-
-  /**
-   * To wait for an HTTP or TCP endpoint before considering the server running, include http or tcp as a protocol. Must be used in conjunction with port.
-   * @default 'tcp'
-   * ```js
-   * module.exports = {
-   *   command: 'npm run start --port 3000',
-   *   protocol: 'http',
-   *   port: 3000,
-   * }
-   * ```
-   */
-  protocol?: 'https' | 'http' | 'tcp' | 'socket';
-
-  /**
-   * Port to wait for activity on before considering the server running. If not provided, the server is assumed to immediately be running.
-   * @default null
-   *
-   * ```js
-   * module.exports = {
-   *   command: 'npm run start --port 3000',
-   *   port: 3000,
-   * }
-   * ```
-   */
-  port: number;
-
-  /**
-   * It defines the action to take if port is already used:
-   * @default 'ask'
-   *
-   * - ask: a prompt is shown to decide if you want to kill the process or not
-   * - error: an errow is thrown
-   * - ignore: your test are executed, we assume that the server is already started
-   * - kill: the process is automatically killed without a prompt
-   *
-   * ```js
-   * module.exports = {
-   *   command: 'npm run start --port 3000',
-   *   port: 3000,
-   *   usedPortAction: 'kill',
-   * }
-   */
-  usedPortAction: 'ask' | 'error' | 'ignore' | 'kill';
-
-  /**
-   * jest-dev-server uses the wait-on npm package to wait for resources to become available before calling callback.
-   * @default {}
-   *
-   * ```js
-   * module.exports = {
-   *   command: 'npm run start --port 3000',
-   *   port: 3000,
-   *   usedPortAction: 'kill',
-   *   waitOnScheme: {
-   *     delay: 1000,
-   *   },
-   * }
-   */
-  waitOnScheme?: Partial<WaitOnOptions>;
-
-  /**
-   * Options which will be passed down to the spawn of the process
-   */
-  options?: SpawnOptions
-}
-
-const DEFAULT_CONFIG: JestProcessManagerOptions = {
-  command: 'npm run start',
-  debug: false,
-  options: {},
-  launchTimeout: 5000,
-  host: 'localhost',
-  port: 3000,
-  protocol: 'tcp',
-  usedPortAction: 'ask',
-  waitOnScheme: {},
-}
-
 const serverLogPrefixer = new stream.Transform({
   transform(chunk, encoding, callback) {
     this.push(chalk.magentaBright(`[Jest Dev server] ${chunk.toString()}`))
@@ -184,9 +57,6 @@ const serverLogPrefixer = new stream.Transform({
   },
 })
 
-export const ERROR_TIMEOUT = 'ERROR_TIMEOUT'
-export const ERROR_PORT_USED = 'ERROR_PORT_USED'
-export const ERROR_NO_COMMAND = 'ERROR_NO_COMMAND'
 export class JestDevServerError extends Error {
   code?: string
   constructor(message: string, code?: string) {
@@ -229,7 +99,6 @@ function runServer(config: JestProcessManagerOptions, index: number) {
   })
 
   if (config.debug) {
-    // eslint-disable-next-line no-console
     console.log(chalk.magentaBright('\nJest dev-server output:'))
     servers[index].stdout!.pipe(serverLogPrefixer).pipe(process.stdout)
   }
@@ -239,8 +108,7 @@ async function outOfStin<T>(block: () => Promise<T>) {
   const { stdin } = process
   const listeners = stdin.listeners('data')
   const result = await block()
-  // @ts-ignore
-  listeners.forEach(listener => stdin.on('data', listener))
+  listeners.forEach(listener => stdin.on('data', listener as (...args: any[]) => void))
   stdin.setRawMode(true)
   stdin.setEncoding('utf8')
   stdin.resume()
